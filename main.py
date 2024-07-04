26f1d495a01192ccddb431b6f2704e78dc96839f
nguyenpavel <nguyenpavel0@gmail.com>
2024-07-04 23:11:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -251,11 +251,11 @@ with container.container():
     cols = container.columns(3)
     
     #Pavel
-    #st.session_state.Custom = True
-    #if (st.session_state['continue_at'] and st.session_state['continue_clip_id']) or st.session_state['prompt_input']:
-    #    Custom = cols[0].toggle(i18n("Custom"), True)
-    #else:
-    #    Custom = cols[0].toggle(i18n("Custom"))
+    st.session_state.Custom = True
+    if (st.session_state['continue_at'] and st.session_state['continue_clip_id']) or st.session_state['prompt_input']:
+        Custom = cols[0].toggle(i18n("Custom"), True)
+    else:
+       Custom = cols[0].toggle(i18n("Custom"))
         
     #st.session_state.TuGeYue = False
     #TuGeYue = cols[1].toggle(i18n("Images TuGeYue Music"))
@@ -425,14 +425,14 @@ with container.container():
             Prompt = container.text_area(label=i18n("Prompt"), value=st.session_state['prompt_input'], placeholder=i18n("Prompt Placeholder"), height=150, max_chars=3000, help=i18n("Prompt Desc"), key="change_prompt", on_change=change_prompt)
         st.session_state.Prompt = Prompt
     #Pavel
-    #else:
-    #    st.session_state.Custom = False
+    else:
+        st.session_state.Custom = False
 
-    #    if 'DescPrompt' not in st.session_state:
-    #        st.session_state.DescPrompt = ""
+        if 'DescPrompt' not in st.session_state:
+            st.session_state.DescPrompt = ""
 
-    #    DescPrompt = container.text_area(label=i18n("Desc Prompt"), value=st.session_state.DescPrompt, placeholder=i18n("Desc Value"), height=150, max_chars=200, help=i18n("Desc Reamrk"), key="change_desc_prompt", on_change=change_desc_prompt)
-    #    st.session_state.DescPrompt = DescPrompt
+        DescPrompt = container.text_area(label=i18n("Desc Prompt"), value=st.session_state.DescPrompt, placeholder=i18n("Desc Value"), height=150, max_chars=200, help=i18n("Desc Reamrk"), key="change_desc_prompt", on_change=change_desc_prompt)
+        st.session_state.DescPrompt = DescPrompt
 
 with container.container():
     cols = container.columns(2)
